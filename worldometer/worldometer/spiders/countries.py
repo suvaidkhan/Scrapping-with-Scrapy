df1124a814b93616cde08c6fc7d7cfa23ebbe466
suvaid.khan <suvaid.khan@globallogic.com>
2021-07-02 01:00:52 +0530

    Second commit added tags

--- worldometer/worldometer/spiders/countries.py
+++ worldometer/worldometer/spiders/countries.py
@@ -3,8 +3,28 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/world-population/population-by-country']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        pass
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            yield response.follow(url=link, callback=self.country_parse, meta={'country_name': name})
+
+    def country_parse(self, response):
+        name = response.request.meta['country_name']
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed "
+                              "table-list'])[1]//tbody//tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+
+            yield {
+                'country name': name,
+                'year': year,
+                'population' : population
+            }
+
